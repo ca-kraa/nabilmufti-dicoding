34683a161ee07e334fb1165f201d7ee25d2bb231
ca-kraa <alexandrokingnabil@gmail.com>
2023-10-28 10:46:12 +0700

    Nabil Mufti - Submission

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('day.csv')
+
+st.title('Streamlit Dashboard - Submission Dicoding')
+
+st.sidebar.header('Options')
+option = st.sidebar.selectbox('Select an Option', ('Show Data', 'Show Visualization'))
+
+
+if option == 'Show Data':
+    st.subheader('Raw Data')
+    st.write(data)
+
+elif option == 'Show Visualization':
+    st.subheader('Data Visualization')
+
+    column_to_visualize = st.selectbox('Select a Column for Visualization', data.columns)
+    if data[column_to_visualize].dtype == 'int64' or data[column_to_visualize].dtype == 'float64':
+        st.write(f'Visualization of {column_to_visualize}')
+        plt.bar(data.index, data[column_to_visualize])
+        plt.xlabel('Index')
+        plt.ylabel(column_to_visualize)
+        st.pyplot(plt)
+
+st.sidebar.write("Created by Nabil Mufti")
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Submissions Dicoding Task - Belajar Analisis Data dengan Python
+
+28 October 2023
+
+- Name : Nabil Mufti
+- Dicoding Account : [Click Here](https://www.dicoding.com/users/nabilmufti/academies)
+- Data Analytics Project : Bike Sharing Dataset
