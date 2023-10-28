cf806f5f561af6afdc922a544cf6805ea1912caf
ca-kraa <alexandrokingnabil@gmail.com>
2023-10-28 10:49:47 +0700

    Nabil Mufti - Submission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 data = pd.read_csv('day.csv')
 
@@ -8,7 +7,6 @@ st.title('Streamlit Dashboard - Submission Dicoding')
 
 st.sidebar.header('Options')
 option = st.sidebar.selectbox('Select an Option', ('Show Data', 'Show Visualization'))
-
 
 if option == 'Show Data':
     st.subheader('Raw Data')
@@ -20,9 +18,6 @@ elif option == 'Show Visualization':
     column_to_visualize = st.selectbox('Select a Column for Visualization', data.columns)
     if data[column_to_visualize].dtype == 'int64' or data[column_to_visualize].dtype == 'float64':
         st.write(f'Visualization of {column_to_visualize}')
-        plt.bar(data.index, data[column_to_visualize])
-        plt.xlabel('Index')
-        plt.ylabel(column_to_visualize)
-        st.pyplot(plt)
+        st.bar_chart(data[column_to_visualize])
 
 st.sidebar.write("Created by Nabil Mufti")
